f049ac574b9be5141d4c2a5809c4b94f2582ab70
Jan Haans <jan.haans@gmail.com>
2023-12-08 16:09:41 +0100

    new receipe endpoint

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/janhaans/recipe-api/controllers"
+)
 
 func main() {
 	router := gin.Default()
+	router.POST("/recipe", controllers.NewRecipeHandler)
 	router.Run()
 }

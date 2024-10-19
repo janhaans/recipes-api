5e36fead0e4584d09b88aca8bd229f87f89fd4cb
Jan Haans <jan.haans@gmail.com>
2024-10-19 21:26:16 +0200

    Added new recipe

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"net/http"
 	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
@@ -17,7 +20,29 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
  }
 
+ var recipes []Recipe
+
+ func init() {
+	recipes = make([]Recipe, 0)
+ }
+
+
+func NewRecipeHandler(c *gin.Context) {
+	var newRecipe Recipe
+	if err := c.ShouldBindJSON(&newRecipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	newRecipe.ID = xid.New().String()
+	newRecipe.PublishedAt = time.Now()
+	recipes = append(recipes, newRecipe)
+	// Here you would typically save the newRecipe to a database
+	c.JSON(201, newRecipe)
+}
+
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }

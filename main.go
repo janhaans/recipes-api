596802749df8922eab1b5f1157a96b8529fa979e
Jan Haans <jan.haans@gmail.com>
2024-10-19 15:23:40 +0200

    back to boilerplate

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/janhaans/recipe-api/controllers"
 )
 
 func main() {
 	router := gin.Default()
-	router.POST("/recipes", controllers.NewRecipeHandler)
-	router.GET("/recipes", controllers.ListRecipesHandler)
-	router.PUT("recipes/:id", controllers.UpdateRecipeHandler)
-	router.DELETE("recipes/:id", controllers.DeleteRecipeHandler)
-	router.GET("recipes/search", controllers.SearchRecipesHandler)
 	router.Run()
 }
